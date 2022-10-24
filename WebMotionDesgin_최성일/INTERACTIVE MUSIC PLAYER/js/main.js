213dbaee3e036b1ad7ec8b386f946e86cfccb71b
yuu <22yuu@naver.com>
2022-10-25 00:26:53 +0900

    style: prev, next button hover animation & add click event

--- WebMotionDesgin_최성일/INTERACTIVE MUSIC PLAYER/js/main.js
+++ WebMotionDesgin_최성일/INTERACTIVE MUSIC PLAYER/js/main.js
@@ -1,5 +1,8 @@
 const frame = document.querySelector('section');
 const list = frame.querySelectorAll('article');
+const prev = document.querySelector('.btnPrev');
+const next = document.querySelector('.btnNext');
+let num = 0;
 const len = list.length;
 const deg = 360 / len; 
 
@@ -7,3 +10,11 @@ for(let i = 0; i < len; i++) {
     list[i].style.transform = `rotate(${deg*i}deg) translateY(-100vh)`;
     // i마다 deg 정도 rotate! 또한 transform의 의해 회전이 되거나 변형이 되어도 축은 변하지 않음 그래서 translateY로 위로 요소를 보내면 회전한 방향에서 바로 자기자신 위로 수직으로 올라가게 됨
 }
+
+prev.addEventListener('click', () => {
+    frame.style.transform = `rotate(${deg* ++num}deg)`;
+})
+
+next.addEventListener('click', () => {
+    frame.style.transform = `rotate(${deg* --num}deg)`;
+})
