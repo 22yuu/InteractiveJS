75b50797f302340d9f7b0c918128d74f059e4e49
yuu <22yuu@naver.com>
2022-10-26 00:01:22 +0900

    feat: SVG UI - show next panel

--- WebMotionDesgin_최성일/SVG UI/js/main.js
+++ WebMotionDesgin_최성일/SVG UI/js/main.js
@@ -1,11 +1,26 @@
 const btnUp = document.querySelector('.btnUp');
+const btnDown = document.querySelector('.btnDown');
 const panel = document.querySelector('.panel');
 const panel_li = panel.querySelectorAll('li');
 const len = panel_li.length - 1;
+let enableClick = true;
 
 btnUp.addEventListener('click', e => {
     e.preventDefault();
 
+    if(enableClick) {
+        enableClick = false;
+        moveUp();
+    }
+})
+
+btnDown.addEventListener('click', e => {
+    if(enableClick) {
+        enableClick = false;
+        moveDown();
+    }
+})
+function moveUp() {
     let current_item = panel.querySelector('.on');
     let current_index = parseInt(current_item.getAttribute('data-index'));
     let next_index = null;
@@ -20,10 +35,25 @@ btnUp.addEventListener('click', e => {
         panel_li[next_index].classList.remove('down');
         panel_li[next_index].classList.add('on');
         panel.querySelector('.up').classList.remove('up');
-    }, 500);
+        enableClick = true;
+    }, 800);
+}
 
-    
+function moveDown() {
+    let current_item = panel.querySelector('.on');
+    let current_index = parseInt(current_item.getAttribute('data-index'));
+    let prev_index = null;
 
+    (current_index !== 0) ? prev_index = current_index - 1 : prev_index = len;
+    console.log(current_index);
+    current_item.classList.remove('on');
+    current_item.classList.add('down');
 
-
-})
+    panel_li[prev_index].classList.add('up');
+    setTimeout(()=> {
+        panel_li[prev_index].classList.remove('up');
+        panel_li[prev_index].classList.add('on');
+        panel.querySelector('.down').classList.remove('down');
+        enableClick = true;
+    }, 800);
+}
